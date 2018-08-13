c22718ad011f564fa7f9fe3973b931892a88314f
Shu <ksc5345@gmail.com>
2018-08-13 18:27:54 +0900

    confirm .ignore file can or not in Gitkraken

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include "stdio.h"
 
 using namespace std;
 
 int main(void) {
 
 	cout << "Hello World" << endl;
+	printf("printf");
+	cout << "Change Point" << endl;
 
 	return 0;
 }
